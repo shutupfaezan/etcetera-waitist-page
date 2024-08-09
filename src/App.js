56eeb4b0bad0b2997d48d6f45a663e5174155a5e
shutup.faezan <faezanmakani13@gmail.com>
2024-08-10 03:16:10 +0530

    trail round two

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <div className="d-flex text-center">
       <h2>We're on maintenance break and too busy to build a screen to show that. Touche huh!</h2>
+      <br/>
+      <h4>COme back again and checkup on us</h4>
     </div>
   );
 }
