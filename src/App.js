5bb2cf84d4b93efe258f81ee00b5047e04d4b9a6
shutup.faezan <faezanmakani13@gmail.com>
2024-08-10 03:01:40 +0530

    new commit to master page im lazy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div>
-
+    <div className="d-flex text-center">
+      <h2>We're on maintenance break and too busy to build a screen to show that. Touche huh!</h2>
     </div>
   );
 }
