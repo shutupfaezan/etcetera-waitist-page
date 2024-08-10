2e76f9cc53e5ba3ad0c8e115d2eb6fc0f42a4086
shutup.faezan <faezanmakani13@gmail.com>
2024-08-10 13:09:33 +0530

    new commit to tceteratech

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="d-flex text-center flex-column vh-100 justify-content-center align-items-center">
       <h2>We're on maintenance break and too busy to build a screen to show that. Touche huh!</h2>
-      <h4>COme back again and checkup on us</h4>
+      <h4>Come back again and checkup on us</h4>
     </div>
   );
 }
