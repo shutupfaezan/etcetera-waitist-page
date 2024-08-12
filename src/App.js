3bed81eeb1f152afcec1c79e66cb2cfafdf77f8b
shutup.faezan <faezanmakani13@gmail.com>
2024-08-13 03:05:13 +0530

    new webiste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Mainpage from "./Mainpage"
 
 function App() {
   return (
-    <div className="d-flex text-center flex-column vh-100 justify-content-center align-items-center">
-      <h2>We're on maintenance break and too busy to build a screen to show that. Touche huh!</h2>
-      <h4>Come back again and checkup on us</h4>
-    </div>
+  <>
+  <Mainpage/>
+  </>
   );
 }
 
---
+++ src/Mainpage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./Mainpage.css"
+
+export default function Mainpage() {
+  return (
+    <div class="bg-purple" style={{height: "100vh"}}>
+        <div class="stars">
+            <div class="custom-navbar">
+                <div class="brand-logo">
+                    <img src={process.env.PUBLIC_URL + "/images/ETCLOGO.png"} width="120px" style={{filter: "invert(100%)"}}/>
+                </div>
+            </div>
+            <div class="central-body d-flex flex-column position-absolute" style={{top: "50%", width: "100%", left: "50%",
+        transform: "translate(-50%, -50%)"}}>
+                <div>
+                    <h1 style={{color: "white", fontWeight: "900"}}>WE'RE ON A BREAK!</h1>
+                    <h1  style={{color: "white", fontWeight: "900"}}>Come back later maybe, once the miantenance is done</h1>
+                </div>
+                {/* <img class="image-404" src={process.env.PUBLIC_URL + "/images/BREAK.png"} width="300px"/>
+                <img class="image-404" src={process.env.PUBLIC_URL + "/images/BREAK.png"} width="300px"/> */}
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"/>
+                <div class="earth-moon">
+                    <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"/>
+                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"/>
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"/>
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
